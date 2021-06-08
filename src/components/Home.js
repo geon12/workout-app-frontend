42e075ece0a1f417083f4607ddd4b9be6b883e39
Cameron <camcreel@gmail.com>
2021-06-08 16:59:53 -0400

    Add components folder and files

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Home() {
+    return (
+        <div>
+            <h1>Workout Creator</h1>
+            <h2>Create Workouts and Exercises</h2>
+        </div>
+    )
+}
+
+export default Home
