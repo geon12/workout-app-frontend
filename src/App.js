00ca1da9c7b4d6687609ff51247563a22b35c225
Cameron <camcreel@gmail.com>
2021-06-08 19:23:47 -0400

    Fetch exercises

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Home from './components/Home';
 
 
 function App() {
+  const [exercises,setExercises] = useState([])
   useEffect(() => {
-    fetch()
+    fetch(`${process.env.REACT_APP_API_URL}/exercises`)
+      .then(resp => resp.json())
+      .then(setExercises)
   },[])
   return (
     <div>
