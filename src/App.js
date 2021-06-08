2f3860804ebec88a72d5cd748fd7053b740fc451
Cameron <camcreel@gmail.com>
2021-06-08 17:18:02 -0400

    Add files to track enviornmental variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useEffect } from 'react';
 import './App.css';
 import Home from './components/Home';
 
 
 function App() {
+  useEffect(() => {
+    fetch()
+  },[])
   return (
     <div>
       <Home />
