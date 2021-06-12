290d2ff9e8825709ccf4471c1fe6044b9adfa35d
Cameron <camcreel@gmail.com>
2021-06-12 15:21:10 -0400

    Add content to Home/Welcome page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,12 @@ function Home() {
         <div>
             <h1>Workout Creator</h1>
             <h2>Create Workouts and Exercises</h2>
+            <ul>
+                <li>Explore different exercises and activities</li>
+                <li>Add new exercises</li>
+                <li>Exercise to premade workout routines</li>
+                <li>Create your own Workout</li>
+            </ul>
         </div>
     )
 }
