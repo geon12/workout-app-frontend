28f43d4a3db43ae3ff19e65d2f36c60200e2ed0d
Cameron <camcreel@gmail.com>
2021-06-12 22:25:14 -0400

    styled homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import React from "react"
 function Home() {
     return (
         <div>
-            <h1>Workout Creator</h1>
-            <h2>Create Workouts and Exercises</h2>
-            <ul>
-                <li>Explore different exercises and activities</li>
-                <li>Add new exercises</li>
-                <li>Exercise to premade workout routines</li>
-                <li>Create your own Workout</li>
+            <h1 className="display-1 mx-4">Workout Creator</h1>
+            <h2 className="display-3 text-muted mx-4">Create Workouts and Exercises</h2>
+            <ul className="list-group">
+                <li className="list-group-item m-4 h3">Explore different exercises and activities</li>
+                <li className="list-group-item m-4 h3">Add new exercises</li>
+                <li className="list-group-item m-4 h3">Exercise to premade workout routines</li>
+                <li className="list-group-item m-4 h3">Create your own Workout</li>
             </ul>
         </div>
     )
